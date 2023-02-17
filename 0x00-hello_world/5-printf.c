101dec7530f49c3f41a32a1b0cba00f8954d99a2
Ceedymanjr <zulyadainiabdurrahman@gmail.com>
2023-02-16 17:16:48 -0800

    C task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - this is the actual funtion
+ *
+ * Return: always is 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+
+}
