a36494b9a9375d3bbc27fa37c170f8f7b18e77e7
Ceedymanjr <zulyadainiabdurrahman@gmail.com>
2023-02-19 17:13:35 -0800

    Task 6 print numbz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * maim - main function
+ *
+ * Return: 0  times
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ;  i++)
+		putchar(i + '0');
+	putchar('\n');
+	return(0);
+}
