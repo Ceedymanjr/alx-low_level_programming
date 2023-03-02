8525b71b77dd0dbf51f4590e7792ead6efc48ee3
Ceedymanjr <zulyadainiabdurrahman@gmail.com>
2023-03-02 13:31:19 -0800

    Task 3 strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,20 +9,15 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int j;
+	char *ss1 = s1;
+	char *ss2 = s2;
 
-	for (j = 0; s1[j] != '\0' || s2[j] != '\0';)
+
+	while (*ss1 != '\0' && *ss2 != '\0' && *ss1 == *ss2)
 	{
-		if (s1[j] != s2[j])
-		{
-			if (s1[j] < s2[j])
-				return (s1[j] - s2[j]);
-			else if (s1[j] > s2[j])
-				return (s1[j] - s2[j]);
-		}
-		else
-			return (0);
+		ss1++;
+		ss2++;
 	}
-	return (0);
+	return (*ss1 - *ss2);
 
 }
