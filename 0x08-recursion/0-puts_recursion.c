4dc4aab7960ba93c3ae8eb7d91b3a58713114cb7
Ceedymanjr <zulyadainiabdurrahman@gmail.com>
2023-03-08 17:38:02 -0800

    Task 1 pint rev recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _puts_recursion -  function that prints a string, followed by a new line
+ * @s: character parameter
+ * Return: always 0
+ */
+void _puts_recursion(char *s);
+{
+	_puts_recursion;
+	puts("recursion");
+	return (0);
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,4 @@
+0x08. C - Recursion
+You mustn't be afraid to dream a little bigger, darling
+Once an idea has taken hold of the brain it's almost impossible to eradicate
+
