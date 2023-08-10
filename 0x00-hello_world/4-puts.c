b92b1fcb4fede9738c1d0befe85ca0e4b27c901c
Ceedymanjr <zulyadainiabdurrahman@gmain.com>
2023-08-10 09:41:08 -0700

    i dont like u betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 
 
+/**
+ * main - this is main entry point
+ *
+ * Return: Always 0
+ */
+
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
