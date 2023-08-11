aa8005ce4b9e4e4e4a849544acfb5f8be72ffdf2
Ceedymanjr <zulyadainiabdurrahman@gmain.com>
2023-08-11 10:28:10 -0700

    print alphabete

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main entry point
+ *
+ * Return: Always 0
+ */
+
+
+int main(void)
+{
+	int i;
+
+
+	for (i = 'a' ; i <= 'z' ; i++)
+	putchar(i);
+	putchar('\n');
+	return (0);
+}
