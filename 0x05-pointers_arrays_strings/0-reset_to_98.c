018452cd65eb753ae10df8a0b525ed5e6a2cd484
Ceedymanjr <zulyadainiabdurrahman@gmain.com>
2023-08-21 07:37:54 -0700

    0-reset

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function that reset and update the valu
+ *
+ * @n: parameter of the function
+ *
+ * Return: Always 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
