8399d3052e30e05266a4e79e31f5836c1d4a3f98
Ceedymanjr <zulyadainiabdurrahman@gmain.com>
2023-08-21 13:05:55 -0700

    revers sstring

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,15 +9,15 @@
 void rev_string(char *s)
 {
 	int x;
-	int count;
+	char y;
+	int count = 0;
 
 	for (x = 0 ; s[x] != '\0' ; x++)
 		count++;
 	for (x = 0 ; x < count / 2 ; x++)
 	{
-		char y;
 		y = s[x];
 		s[x] = s[count - 1 - x];
-		s[count - 1 - x] = 3;
+		s[count - 1 - x] = y;
 	}
 }
