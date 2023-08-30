0b4f3ba79b2ed032aaa4fbb63806f60cc654e617
Ceedymanjr <zulyadainiabdurrahman@gmain.com>
2023-08-30 15:49:56 -0700

    5-square root

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,12 +1,20 @@
 #include "main.h"
 
 /**
+ * _sqrt_recursive - function that Recursively searches for the square root
+ * @low: parameter
+ * @high: parameter
+ * @n: number
+ * Return: n
+ */
+int _sqrt_recursive(int n, int low, int high);
+
+/**
  * _sqrt_recursion - function that return the square root of numbers
  * @n: number
  * Return: n
  */
 
-int _sqrt_recursive(int n, int low, int high);
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
@@ -16,13 +24,14 @@ int _sqrt_recursion(int n)
 	return (_sqrt_recursive(n, 0, n));
 }
 
-
 /**
- * _sqrt_recursive - function that Recursively searches for the square root
+ * _sqrt_recursive - Recursively searches for the square root of a number
+ * search-like approach within a specified search range
+ * @n: number
  * @low: parameter
  * @high: parameter
- * @n: number
- * Return: n
+ * Return: returns square root if found, or -1
+ * if no exact natural square root exists within the range
  */
 
 int _sqrt_recursive(int n, int low, int high)
