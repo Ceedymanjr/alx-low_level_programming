1cbe09c382fd5f96a43deb5d79a5e92ce8342a00
Ceedymanjr <zulyadainiabdurrahman@gmain.com>
2023-09-06 19:10:55 -0700

    Task2-strconcat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -5,40 +5,30 @@
  * str_concat - function that get ends of input and add together for size
  * @s1: input one to concat
  * @s2: input two to concat
- * Return: concat of s1 and s2
+ * Return: concated pointer of new space
  */
-
 char *str_concat(char *s1, char *s2)
 {
-	char *conct;
-	int i, j;
+	int i;
+	int s1len = 0;
+	int s2len = 0;
+	char *concat;
 
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
-	{
 		s2 = "";
-		i = j = 0;
-	}
-	while (s1[i] != '\0')
-		i++;
-	while (s2[j] != '\0')
-		j++;
-	conct = malloc(sizeof(char) * (i + j + 1));
-	if (conct == NULL)
+	for (i = 0; s1[i] != '\0'; i++)
+		s1len++;
+	for (i = 0; s2[i] != '\0'; i++)
+		s2len++;
+	concat = malloc(sizeof(char) * (s1len + s2len) + 1);
+	if (concat == NULL)
 		return (NULL);
-	i = j = 0;
-	while (s1[i] != '\0')
-	{
-		conct[i] = s1[i];
-		i++;
-	}
-	while (s2[j] != '\0')
-	{
-		conct[i] = s2[j];
-		i++, j++;
-		conct[i] = '\0';
-	}
-	return (conct);
+	for (i = 0; s1[i] != '\0'; i++)
+		concat[i] = s1[i];
+	for (i = 0; s2[i] != '\0'; i++)
+		concat[s1len + 1] = s2[i];
+	return (concat);
 }
