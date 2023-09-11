5b5be117af3e1ef5dafbff57785956273c94deb7
Ceedymanjr <zulyadainiabdurrahman@gmain.com>
2023-09-11 11:00:26 -0700

    Task 0 am all

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - dog structure
+ * @name: name if the dog
+ * @age: age of the dog
+ * @owner: owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
