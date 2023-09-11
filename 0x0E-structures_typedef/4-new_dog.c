b962819e16a843731ab4fcbe24f3f65b9db5b4ea
Ceedymanjr <zulyadainiabdurrahman@gmain.com>
2023-09-11 15:23:45 -0700

    Task 2-print dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stdio.h>
+/**
+ * _strlen - function that find the lenght of the string
+ * @s: string to find the lenght
+ * Return: Always 0
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+		i++;
+	return (0);
+}
+
+
+int *_strcpy(char *dest, *src)
