9415b2ae96a6bac571a546fd0f71029f874c2721
Ceedymanjr <zulyadainiabdurrahman@gmain.com>
2023-09-11 17:54:59 -0700

    Task 2-print dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,12 +6,12 @@
  * @age: age of the dog
  * @owner: owner
  */
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}dog_t;
+};
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif
