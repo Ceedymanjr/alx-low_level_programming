1cd31ef681cd7f6748fe4900e1c853ccc454d60e
Ceedymanjr <zulyadainiabdurrahman@gmain.com>
2023-09-21 15:31:40 -0700

    fist commit all in one

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ * print_list - function that prints all the elements of a list_t list
+ * @h: pointer of the function
+ * Return: number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t node;
+
+	node = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[%d] %s\n", 0, "(nill)");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		node++;
+	}
+	return (node);
+}
